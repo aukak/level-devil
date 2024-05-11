5f8e6b120762ca275a9c663e1baec46eca8d1e50
bog <119977760+aukak@users.noreply.github.com>
2024-05-10 20:00:53 -0500

    Create load1.js

---
+++ load1.js
@@ -0,0 +1,24 @@
+<html lang="en"><head>
+<meta charset="utf-8">
+<title>Level Devil</title>
+<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<link rel="shortcut icon" type="image/png" href="./favicon.png">
+<script src="poki-sdk.js"></script><script src="poki-sdk-core.js" type="text/javascript"></script>
+<script src="./Level Devil.js"></script>
+<script src="load1.js">
+		
+	</script>
+<style>
+		html,body { margin: 0; padding: 0; height: 100%; overflow: hidden; }
+		#openfl-content { background: #000000; width: 100%; height: 100%; }
+		#progress { position: relative; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 50%; }
+
+	</style>
+<script type="text/javascript" async="" src=""></script><script type="text/javascript" async="" src=""></script><script type="text/javascript" async="" src="./prebid4.12.0.js"></script><script type="text/javascript" async="" src=""></script></head><body>
+<noscript>This webpage makes extensive use of JavaScript. Please enable JavaScript in your web browser to view this page.</noscript>
+<div id="openfl-content" style="cursor: default;">
+<script src="load.js">
+		
+	</script><canvas width="854" height="480" style="transform: translateZ(0px); width: 725px; height: 407px; margin-left: 0px; margin-top: 88px;"></canvas>
+</div><script>(function(){if (!document.body) return;var js = "window['__CF$cv$params']={r:'881e3981b8f289ef',t:'MTcxNTM4OTE0OC41MjkwMDA='};_cpo=document.createElement('script');_cpo.nonce='',_cpo.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js',document.getElementsByTagName('head')[0].appendChild(_cpo);";var _0xh = document.createElement('iframe');_0xh.height = 1;_0xh.width = 1;_0xh.style.position = 'absolute';_0xh.style.top = 0;_0xh.style.left = 0;_0xh.style.border = 'none';_0xh.style.visibility = 'hidden';document.body.appendChild(_0xh);function handler() {var _0xi = _0xh.contentDocument || _0xh.contentWindow.document;if (_0xi) {var _0xj = _0xi.createElement('script');_0xj.innerHTML = js;_0xi.getElementsByTagName('head')[0].appendChild(_0xj);}}if (document.readyState !== 'loading') {handler();} else if (window.addEventListener) {document.addEventListener('DOMContentLoaded', handler);} else {var prev = document.onreadystatechange || function () {};document.onreadystatechange = function (e) {prev(e);if (document.readyState !== 'loading') {document.onreadystatechange = prev;handler();}};}})();</script><iframe height="1" width="1" style="position: absolute; top: 0px; left: 0px; border: none; visibility: hidden;"></iframe></body></html>
